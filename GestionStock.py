9ca7821161e318f3999fec6ab37271a565bd2e0d
Carbouba <saniboubacar1992@gmail.com>
2026-01-03 10:14:39 +0100

    GesStock_V0.1

--- GestionStock.py
+++ GestionStock.py
@@ -3,10 +3,13 @@ def voir_stock():
     if total_stock == 0:
         print(f"Aucun produit disponible pour le moment !")
     else:
-        print("\n===== Liste des produits en stock =====")
+        print("\n===== Liste des produits en stock =====\n")
+        print("-------------------------------")
+        print(f"Produits\t|\tStocks")
+        print("-------------------------------")
         for stck, qnte in stocks.items():
-            print(f"{stck.capitalize():5} : {qnte:>5}")
-        print("====================\n")
+            print(f"{stck.capitalize():<15} : {qnte:>5}")
+        #print("_________________________________")
     
     return
 def ajout_stock():
@@ -16,35 +19,57 @@ def ajout_stock():
             if nom_prod == "retour":
                 return
             if nom_prod == "":
-                print("❌ Erreur : Vous devez saisir un nom correct : ")
+                print("\n❌ Erreur : Vous devez saisir un nom correct : ")
                 continue
             
             if nom_prod in stocks:
-                quantite = int(input(f"Il y en a deja {stocks[nom_prod]}, combien voulez-vous en ajouter : "))
+                quantite = int(input(f"\nIl y en a deja {stocks[nom_prod]}, combien voulez-vous en ajouter : "))
                 while True:
                     if quantite <= 0:
-                        quantite = int(input("❌ Erreur : Entrez une valeur positive : "))
+                        quantite = int(input("\n❌ Erreur : Entrez une valeur positive : "))
                     else:
                         stocks[nom_prod] += quantite
-                        print("✅ Quantité ajouter !")
+                        print("\n✅ Quantité ajouter !")
                         return
             else:
-                quantite = int(input("Entrez la quantité : "))
+                quantite = int(input("\nEntrez la quantité : "))
                 while True:
                     if quantite <= 0:
-                        quantite = int(input("❌ Erreur : Entrez une valeur positive : "))
+                        quantite = int(input("\n❌ Erreur : Entrez une valeur positive : "))
                     else:
-                        stocks[nom_prod.lower()] = quantite
-                        print("✅ Le produit a ete ajouté ➕!")
+                        stocks[nom_prod] = quantite
+                        print("\n✅ Le produit a ete ajouté ➕!")
                         return
         except ValueError:
             print("\n❌ Erreur : Vous devez entré un  CHIFFRE, pas une lettre.")
+
 def vendre_produit():
+    total_stock = len(stocks)
+    if total_stock == 0:
+        print( "\nAucun produit à vendre pour le moment !\n")
+    else:
+        prod_vend = input("\nQuel produit souhaitez vous vendre : ").strip().lower()
+        if prod_vend not in stocks:
+            print("\n❌ Erreur : ce produit n'existe pas !")
+            return
+        else:
+            while True:
+                try:
+                    qunt_vend = int(input("\nCombien voulez vous vendre : "))
+                    stock_dispo = stocks[prod_vend]
+                    if qunt_vend > stock_dispo:
+                        print("\n❌ Erreur : vous avez depassez la quantité disponible en stock !")
+                        continue
+                    else:
+                        stocks[prod_vend] -= qunt_vend
+                        print("\n✅ Produit vendu ")
+                        if stocks[prod_vend] == 0:
+                            stocks.pop(prod_vend, None)
+                    return
+                except ValueError:
+                    print("\n❌ Erreur : Vous devez entré un  CHIFFRE, pas une lettre.")
+                    continue
     
-
-    return
-    
-
 stocks = {}
 
 # Programme principale
